430eddc5a5d8d65d5237dcd40ea4dcc41cf0f0e6
hanxiong li <hanxiong.li98@gmail.com>
2024-03-12 14:55:23 +0100

    basic_pitch

---
+++ script/url_to_mp3.py
@@ -0,0 +1,18 @@
+import youtube_dl
+import os
+
+def download_video(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+if __name__ == "__main__":
+    video_url = input("请输入视频的URL: ")
+    download_video(video_url)
